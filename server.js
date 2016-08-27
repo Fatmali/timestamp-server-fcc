413373f8c6f07a7daba66bd2634b3fa641cb5665
Fatma <tumz92@gmail.com>
2016-08-27 22:25:30 +0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express=require('express');
+
+var app=express();
+
+app.get('/',function(req,res){
+
+	res.writeHead(200,{"Content-Type":"text/plain"});
+	res.send("Hello! World");
+	res.end();
+});
+
+
+app.listen(8080);
