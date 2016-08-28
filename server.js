2b010d1653cc7ed452aae06e9cbb557b415dfe53
Fatma <tumz92@gmail.com>
2016-08-28 22:24:11 +0300

    This is a new commit for what I originally planned to be amended

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express=require('express');
 var time=require('./time');
 var app=express();
 
+var path    = require("path");
+var port = process.env.PORT || 8080;
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
 app.get('/:date',function(req,res){
 
 	var date=req.params.date;
