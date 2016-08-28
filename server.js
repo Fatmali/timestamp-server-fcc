10b9fb4e91cc14cfa63ff39c365b04b1e825eb0d
Fatma <tumz92@gmail.com>
2016-08-28 18:08:18 +0300

    Final Commit

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 var express=require('express');
-var url=require('url');
-
+var mongo=require('mongodb').MongoClient;
+var time=require('./time');
+var url='mongodb://localhost:27017/timestampdb'
 var app=express();
 
-app.get('/',function(req,res){
+app.get('/:date',function(req,res){
+
+	var date=req.params.date;
+	var response="empty";
+
+	//if date is unix date
+	if(/^\d{1,30}$/.test(date)){
+		response=JSON.stringify(time.translateunix(date));
 	
-	res.send("Hello! World");
+	}
+	
+
+
+
+	//if date is natural language
+
+	if(/(January|February|March|April|May|June|July|August|September|October|November|December)\s(\d\d?),\s(\d\d\d\d)/.test(date)){
+		console.log(date);
+		response=JSON.stringify(time.translatenatural(date));
+		
+
+
+}
+
+	res.send(response);
 	res.end();
 });
 
 
 app.listen(8080);
+
